57b536524032ad69a3cf7221f52ceabd11b8b07b
Jorge Renso <jorgerensoraji@hotmail.com>
2025-05-21 01:22:06 -0400

    Primer commit

--- servidor.js
+++ servidor.js
@@ -5,6 +5,10 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
